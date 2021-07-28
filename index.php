0e24d7276e0ef6f184aaf48e1fbf2b698c800c73
oksanka <drobchak.o@gmail.com>
2021-07-28 09:28:41 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$test= 'test';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div class="container">
+    <?php
+     echo $test;
+    ?>
+</div>
+
+</body>
+</html>
+
+
